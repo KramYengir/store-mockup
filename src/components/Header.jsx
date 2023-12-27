2576a7bd856ce8893a5329d47a81e9f57cdf4937
KramYengir <kramyengir@gmail.com>
2023-12-27 16:21:47 +0000

    establish subject & style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
 import "../styles/header.css";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoBodyOutline } from "react-icons/io5";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Link to={"/"}>
+        <div className="logo">
+          <i>
+            <IoBodyOutline />
+          </i>
+          <h1>
+            Yo<span>Body</span>Shop
+          </h1>
+        </div>
+      </Link>
+      <ul>
+        <li>
+          <Link to={"/products"}>Shop</Link>
+        </li>
+        <li>
+          <Link to={"/cart"}>{<FaShoppingCart />}</Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
